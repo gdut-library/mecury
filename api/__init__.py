ee235ab9a79e5bd8cf693a641eba86a2ac61d67f
hbc <bcxxxxxx@gmail.com>
2013-09-19 20:25:54 +0800

    bumped to v0.0.2, happy :moon: festival

--- api/__init__.py
+++ api/__init__.py
@@ -3,8 +3,9 @@
 from .library import *
 from .book import *
 from .me import *
+from .exceptions import *
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'hbc <bcxxxxxx@gmail.com>'
 __homepage__ = 'https://github.com/bcho/gdut-library-api'
